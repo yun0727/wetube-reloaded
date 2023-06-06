09e4ef584b64fc297f59649855eb816311ca5424
yun <austru00@gmail.com>
2023-06-07 00:13:48 +0900

    3.0 Your First Server

---
+++ src/server.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const PORT = 4000;
+
+const app = express();
+
+const handleListening = () => console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
+
+app.listen(PORT,handleListening)
