ec5e50dcf5619cfc4107ce66e57f49fed447a720
yun <austru00@gmail.com>
2023-06-07 12:53:37 +0900

    3.6 Middlewares 2

--- src/server.js
+++ src/server.js
@@ -4,8 +4,8 @@ const PORT = 4000;
 
 const app = express();
 
-const gossipMiddleware = (req, res, next) => {
-  console.log(`Someone is going to : ${req.url}`);
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
   next();
 }
 
@@ -13,7 +13,7 @@ const handleHome = (req, res) => {
   return res.send("I love middlewares");
 };
 
-app.get("/", gossipMiddleware, handleHome);
+app.get("/", logger, handleHome);
 
 const handleListening = () => console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
 
