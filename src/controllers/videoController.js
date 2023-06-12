cbbb1327a2042b48236917df000a07edc63ddc8c
yun <austru00@gmail.com>
2023-06-12 17:57:05 +0900

    5.1 Configuring Pug

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,7 +1,5 @@
 export const trending = (req, res) => 
-  res.send(
-    "<!DOCTYPE html><html lang='ko'><head><title>Wetube</title></head><body><h1>Home</h1><footer>&copy;2023 Wetube -  All rights reserved</footer></body></html>"
-    );
+  res.render("home");
 
 export const see = (req, res) => {
   return res.send(
