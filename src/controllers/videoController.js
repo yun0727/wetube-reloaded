d1871e07da35b01260cc5ac20e017c2671dec42e
yun <austru00@gmail.com>
2023-06-14 12:34:20 +0900

    5.9 Mixins

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,5 +1,25 @@
 export const trending = (req, res) => {
-  const videos = [1,2,3,4,5,6
+  const videos = [{
+      title: "First Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,},
+      {
+        title: "Second Video",
+        rating: 5,
+        comments: 2,
+        createdAt: "2 minutes ago",
+        views: 59,
+        id: 1,},
+        {
+          title: "Third Video",
+          rating: 5,
+          comments: 2,
+          createdAt: "2 minutes ago",
+          views: 59,
+          id: 1,}
   ];
   return res.render("home", { pageTitle: "Home", videos });
 };
