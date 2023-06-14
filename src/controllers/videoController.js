e204f20bcb5eff118507cd1b7084bdab5127f360
yun <austru00@gmail.com>
2023-06-14 13:02:56 +0900

    6.1 Array Database 2

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -29,10 +29,10 @@ export const trending = (req, res) => {
   return res.render("home", { pageTitle: "Home", videos });
 };
 
-export const see = (req, res) => {
+export const watch = (req, res) => {
   const { id } = req.params;
   const video = videos[id - 1];
-  return res.render("watch", { pageTitle: `Watching ${video.title}` });
+  return res.render("watch", { pageTitle: `Watching ${video.title}`,video });
 };
 export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
