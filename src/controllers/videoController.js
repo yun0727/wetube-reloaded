24756d796d9f2ce9529d10c9f6c08eb0c9e55aee
yun <austru00@gmail.com>
2023-06-20 15:02:23 +0900

    6.17 Exceptions and Validation

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -31,20 +31,24 @@ export const postEdit = (req, res) => {
   const { title } = req.body;
   return res.redirect(`/videos/${id}`);
 };
+
 export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: "Upload Video" });
 };
+
 export const postUpload = async (req, res) => {
   const { title, description, hashtags } = req.body;
-  await Video.create({
-    title,
-    description,
-    createdAt: Date(now),
-    hashtags: hashtags.split(",").map((word) => `#${word}`),
-    meta: {
-      views: 0,
-      rating: 0,
-    },
-  });
-  return res.redirect("/");
+  try {
+    await Video.create({
+      title,
+      description,
+      hashtags: hashtags.split(",").map((word) => `#${word}`),
+    });
+    return res.redirect("/");
+  } catch (error) {
+    return res.render("upload", {
+      pageTitle: "Upload Video",
+      errorMessage: error._message,
+    });
+  }
 };
