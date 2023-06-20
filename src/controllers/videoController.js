c5cd77980868306ac3497a6f99108b4275623593
yun <austru00@gmail.com>
2023-06-20 14:05:14 +0900

    6.12 Our First Query 2

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,32 +1,31 @@
 import Video from "../models/Video"
 
-
 export const home = (req, res) => {
-  Video.find({},(error, videos)=>{});
-  return res.render("home", { pageTitle: "Home" });
+  console.log("Start");
+  Video.find({}, (error, videos) => {
+    console.log("Finished");
+    return res.render("home", { pageTitle: "Home", videos });
+  });
+  console.log("I finish first");
 };
 
 export const watch = (req, res) => {
   const { id } = req.params;
-  return res.render("watch", { pageTitle: `Watching`});
+  return res.render("watch", { pageTitle: `Watching` });
 };
-
 export const getEdit = (req, res) => {
   const { id } = req.params;
-  return res.render("edit", { pageTitle: `Editing`});
+  return res.render("edit", { pageTitle: `Editing` });
 };
-
 export const postEdit = (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
   return res.redirect(`/videos/${id}`);
 };
-
-export const getUpload = (req, res) =>{
-  return res.render("upload", {pageTitle:"Upload Video"});
+export const getUpload = (req, res) => {
+  return res.render("upload", { pageTitle: "Upload Video" });
 };
-
-export const postUpload = (req, res) =>{
+export const postUpload = (req, res) => {
   const { title } = req.body;
-  return res.redirect('/')
-}
+  return res.redirect("/");
+};
