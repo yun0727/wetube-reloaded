c986b0b9fa32a517fc34eb655ae3207b8a041ad9
yun <austru00@gmail.com>
2023-06-24 15:59:41 +0900

    6.27 Search 2

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -60,9 +60,16 @@ export const deleteVideo = async (req, res) => {
   return res.redirect("/");
 };
 
-export const search=(req,res)=>{
-  const {keyword} =req.query
-  if (keyword){}
-  
-  return res.render("search", {pageTitle:"Search"})
+export const search=async(req,res)=>{
+  const {keyword} =req.query;
+  let videos=[];
+  if (keyword) {
+    videos = await Video.find({
+      title: {
+        $regex: new RegExp(`${keyword}$`, "i")
+      },
+    });
+  }
+
+  return res.render("search", {pageTitle:"Search", videos}  )
 }
