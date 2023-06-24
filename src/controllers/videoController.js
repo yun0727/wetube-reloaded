38655438f971a755188941c31fc5c35b01dc75d6
yun <austru00@gmail.com>
2023-06-24 15:05:36 +0900

    6.24 Statics

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,16 +1,4 @@
-import { now } from "mongoose";
 import Video from "../models/Video"
-
-/*
-console.log("start")
-Video.find({}, (error, videos) => {
-  if (error){
-    return res.render("server-error")
-  }
-  return res.render("home", { pageTitle: "Home", videos });
-});
-console.log("finished")
-*/
 
 export const home = async (req, res) => {
   const videos = await Video.find({});
@@ -21,7 +9,7 @@ export const watch = async(req, res) => {
   const { id } = req.params;
   const video = await Video.findById(id);
   if (!video){
-    return res.render("404", {pageTitle:"Video not found"})
+    return res.render("404", {pageTitle:"Video not found."})
   }
   return res.render("watch", {pageTitle:video.title, video})
 };
@@ -45,7 +33,7 @@ export const postEdit = async(req, res) => {
   await Video.findByIdAndUpdate(id, {
     title, 
     description,
-    hashtags: hashtags.split(",").map((word)=>(word.startsWith("#")?word:`#${word}`)),
+    hashtags: Video.formatHashtags(hashtags),
   })
   return res.redirect(`/videos/${id}`);
 };
@@ -60,7 +48,7 @@ export const postUpload = async (req, res) => {
     await Video.create({
       title,
       description,
-      hashtags,
+      hashtags:Video.formatHashtags(hashtags),
     });
     return res.redirect("/");
   } catch (error) {
