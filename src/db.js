6bea143b832e5e2142f45927a92fd79ab72137be
yun <austru00@gmail.com>
2023-06-24 16:24:24 +0900

    7.1 Create Acount 2

--- src/db.js
+++ src/db.js
@@ -1,6 +1,9 @@
 import mongoose from "mongoose"
 
-mongoose.connect("mongodb://127.0.0.1:27017/wetube", {useNewUrlParser:true, useUnifiedTopology:true,useFindAndModify:false,});
+mongoose.connect("mongodb://127.0.0.1:27017/wetube", {useNewUrlParser:true, 
+useUnifiedTopology:true,
+useFindAndModify:false,
+useCreateIndex:true,});
 
 const db = mongoose.connection;
 
