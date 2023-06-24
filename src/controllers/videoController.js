8e3cfb389f3cbf80d48734f59665d203b226882b
yun <austru00@gmail.com>
2023-06-24 15:14:30 +0900

    6.25 Delete Video

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -58,3 +58,11 @@ export const postUpload = async (req, res) => {
     });
   }
 };
+
+export const deleteVideo=async(req, res)=>{
+  const {id} = req.params;
+  await Video.findByIdAndDelete(id)
+
+  return res.redirect ("/")
+
+}
