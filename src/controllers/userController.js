ff26caaf465f336f8761c9070456067003f8e1d4
yun <austru00@gmail.com>
2023-06-26 15:29:26 +0900

    7.5 Login 1

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -15,10 +15,10 @@ export const postJoin = async (req, res) => {
   if (exists){
     return res.status(400).render("join",{
       pageTitle,
-      errorMessage: "This username is already taken"
+      errorMessage: "This username/email is already taken"
     })
   }
-
+  try {
   await User.create({
     name,
     username,
@@ -27,10 +27,26 @@ export const postJoin = async (req, res) => {
     location,
   });
   return res.redirect("/login");
+  } catch (error) {
+    return res.status(400).render("join",{
+      pageTitle:"Upload Video",
+      errorMessage:error._message,
+    })
+  }
 };
+
+export const getLogin = (req, res) => res.render("login", {pageTitle:"Login"});
+
+export const postLogin=async(req, res)=>{
+  const {username, password} = req.body;
+  const exists = await User.exists({username});
+  if (!exists){
+    return res.status(400).render("login", {pageTitle:"login", errorMessage:"An account with this username does not exists"});
+  }
+  res.end()
+}
 
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
-export const login = (req, res) => res.send("Login");
 export const logout = (req, res) => res.send("Log out");
 export const see = (req, res) => res.send("See User");
--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,5 @@
 import bcrypt from "bcrypt"
+import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
   email:{type:String, required:true, unique:true},
