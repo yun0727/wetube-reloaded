9ef8152d8b994b3ab84bafa7cb3ca1272c344430
yun <austru00@gmail.com>
2023-06-26 15:58:20 +0900

    7.7 Sessions and Cookies 1

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 
 import express from "express";
 import morgan from "morgan";
-import globalRouter from "./routers/rootRouter";
+import session from "express-session"
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
 import rootRouter from "./routers/rootRouter";
@@ -14,6 +14,22 @@ app.set("view engine", "pug");
 app.set("views",process.cwd()+"/src/views");
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    secret:"Hello",
+    resave:true,
+    saveUninitialized:true,
+  })
+)
+
+app.use((req,res,next)=>{
+  req.sessionStore.all((error, sessions) => {
+    console.log(sessions);
+    next();
+  });
+});
+
 app.use("/",rootRouter);
 app.use("/videos",videoRouter);
 app.use("/users",userRouter);
