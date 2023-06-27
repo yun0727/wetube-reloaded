cd228374f8bd997380221110dbee5e9e85095476
yun <austru00@gmail.com>
2023-06-27 16:28:29 +0900

    7.12 MongoStore

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 
 import express from "express";
 import morgan from "morgan";
-import session from "express-session"
+import session from "express-session";
+import MongoStore from "connect-mongo";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
 import rootRouter from "./routers/rootRouter";
@@ -21,6 +22,7 @@ app.use(
     secret:"Hello",
     resave:true,
     saveUninitialized:true,
+    store: MongoStore.create({ mongoUrl: "mongodb://127.0.0.1:27017/wetube" }),
   })
 )
 
