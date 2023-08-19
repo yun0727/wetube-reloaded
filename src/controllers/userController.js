94b15c93f2d11780a377c4242c4ea3bfe32f3132
yun <austru00@gmail.com>
2023-08-19 16:54:49 +0900

    6.18 Github Login 4

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -60,7 +60,7 @@ export const postLogin=async(req, res)=>{
 export const startGithubLogin = (req, res) => {
   const baseUrl = "https://github.com/login/oauth/authorize";
   const config = {
-    client_id : "e6457655daf8ab2988cd",
+    client_id : process.env.GH_CLIENT,
     allow_signup : false,
     scope: "read:user user:email",
   };
@@ -69,7 +69,24 @@ export const startGithubLogin = (req, res) => {
   return res.redirect(finalUrl);
 }
 
-export const finishGithubLogin = (req, res) => {};
+export const finishGithubLogin = async (req, res) => {
+  const baseUrl = "https://github.com/login/oauth/access_token";
+  const config = {
+    client_id: process.env.GH_CLIENT,
+    client_secret: process.env.GH_SECRET,
+    code: req.query.code,
+  };
+  const params = new URLSearchParams(config).toString();
+  const finalUrl = `${baseUrl}?${params}`;
+  const data= await fetch (finalUrl,{
+    method:"POST",
+    headers:{
+      Accept:"application/json",
+    },
+  });
+  const json = await data.json();
+  console.log(json);
+};
 
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
