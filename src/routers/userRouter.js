57bd91adf74463c5a7341ea3ba523334e6eb1575
yun <austru00@gmail.com>
2023-08-23 01:16:00 +0900

    kakao Login

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { edit, logout, see, startGithubLogin, finishGithubLogin } from "../controllers/userController";
+import { edit, logout, see, startGithubLogin, finishGithubLogin, startKakaoLogin, finishKakaoLogin } from "../controllers/userController";
 
 const userRouter = express.Router();
 
@@ -7,6 +7,8 @@ userRouter.get("/logout", logout);
 userRouter.get("/edit", edit);
 userRouter.get("/github/start", startGithubLogin);
 userRouter.get("/github/finish", finishGithubLogin);
+userRouter.get("/kakao/start", startKakaoLogin);
+userRouter.get("/kakao/finish", finishKakaoLogin);
 userRouter.get(":id", see);
 
 
