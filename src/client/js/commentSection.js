bd3917b24c89021c9de5ffe7abcdc302a2e13dfc
yun <austru00@gmail.com>
2023-09-13 16:22:13 +0900

    16.6 Rendering Comments

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -1,25 +1,23 @@
 const videoContainer = document.getElementById("videoContainer");
 const form = document.getElementById("commentForm");
 
-const handleSubmit = (event) =>{
+const handleSubmit = async (event) => {
   event.preventDefault();
   const textarea = form.querySelector("textarea");
   const text = textarea.value;
   const videoId = videoContainer.dataset.id;
-  if (text === ""){
+  if (text === "") {
     return;
   }
-  fetch(`/api/videos/${videoId}/comment`,{
-    method:"POST",
-    headers:{
-      "Content-type":"application/json",
+  await fetch(`/api/videos/${videoId}/comment`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify({text})
+    body: JSON.stringify({ text }),
   });
   textarea.value = "";
 };
-form.addEventListener("submit", handleSubmit);
-
-if (form){
+if (form) {
   form.addEventListener("submit", handleSubmit);
 }
