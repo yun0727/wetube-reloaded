7540bfe0988b49be451d49087326663189d16fe1
yun <austru00@gmail.com>
2023-10-16 16:20:58 +0900

    location

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -227,6 +227,7 @@ export const postEdit = async(req, res) => {
     body: {name, email, username, location},
     file,
   } = req;
+  console.log(file)
   const updatedUser = await User.findByIdAndUpdate(
     _id,
     {
