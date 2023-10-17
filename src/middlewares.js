489369cfc4906ce0f003941a009b138e89e329e9
yun <austru00@gmail.com>
2023-10-17 14:15:05 +0900

    17.9 Production Environment

--- src/middlewares.js
+++ src/middlewares.js
@@ -9,9 +9,17 @@ const s3 = new aws.S3({
   }
 })
 
-const multerUploader = multerS3({
+const isHeroku = process.env.NODE_ENV === "production";
+
+const s3ImageUploader = multerS3({
   s3 : s3,
-  bucket: 'wetube-2023',
+  bucket: 'wetube-2023/images',
+  acl: "public-read"
+})
+
+const s3VideoUploader = multerS3({
+  s3 : s3,
+  bucket: 'wetube-2023/videos',
   acl: "public-read"
 })
 
@@ -44,13 +52,14 @@ export const avatarUpload = multer({
   limits:{
     fileSize: 3000000,
   },
-  storage: multerUploader,
-})
+  storage: isHeroku ? s3ImageUploader : undefined,
+});
 
 export const videoUpload = multer({
   dest: "uploads/videos/",
 limits:{
   fileSize: 10000000,
 },
-storage: multerUploader,
+storage: isHeroku ? s3VideoUploader: undefined,
 })
+
